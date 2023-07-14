90c6b373affafeb4eae80813c217df2037ada261
Stroescu Diana-Lavinia <stroescudiana@yahoo.com>
2023-07-14 20:00:22 +0300

    Version 1.0

--- Curs_Automation/Teme/Workshop_5_tema/WS5_tema.py
+++ Curs_Automation/Teme/Workshop_5_tema/WS5_tema.py
@@ -1,5 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get("https://www.elefant.ro/")
+
+# # Delete all cookies
+# driver.delete_all_cookies()
+time.sleep(5)
+wait = WebDriverWait(driver, 10)
+cookies_bttn = wait.until(
+    EC.element_to_be_clickable((By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"))
+)
+
+cookies_bttn.click()
+time.sleep(3)
--- pythonProject/test_cases/TestPositionLogin.py
+++ pythonProject/test_cases/TestPositionLogin.py
@@ -1,7 +1,5 @@
 import unittest
 
-import os, sys 
-sys.path.append('./')
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
